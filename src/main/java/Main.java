97fb932455575dc9c554e66020967e3d65e73301
Caroline Rives <car.rives@gmail.com>
2022-07-13 15:58:03 -0400

    Factory Lab completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        // your code here
+        CameraFactory cameraFactory = new CameraFactory();
+        Camera camera = cameraFactory.makeCamera(CameraFactory.CameraManufacturer.CANON_FILM);
+        Photographer photographer = new Photographer(camera);
+
+        photographer.takePhotograph();
     }
 }
